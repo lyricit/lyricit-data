9af7ee6b594def296b74c6e508d6f81812c5c841
heecircle <yhw991228@gmail.com>
2024-03-25 17:51:11 +0900

    feat : add elastic search class for confirm track exist

---
+++ data/track_elastic_search.py
@@ -0,0 +1,51 @@
+"""
+search data from elastic search
+"""
+import os
+import json
+import csv
+from dotenv import load_dotenv
+from elasticsearch import Elasticsearch
+
+
+
+class ElasticAPI:
+    """
+    Elastic search, insert API
+    """
+    load_dotenv()
+    ES_HOST = os.environ.get("ELASTIC_HOST")
+    ES_PORT = os.environ.get("ELASTIC_PORT")
+    client = Elasticsearch(
+        f"http://{ES_HOST}:{ES_PORT}"       
+    )
+    index_name = "tracks"
+    @classmethod
+    def data_isin(cls, track_id_list: list) -> list:
+        """
+        check if index exists
+        """
+        response = cls.client.search(
+            index="tracks",
+            body={
+                "query": {
+                    "terms": {
+                        "code": track_id_list
+                    }
+                }
+            }
+        )
+        json_response = json.loads(json.dumps(response.body))
+        
+        return [ code_resp.get("_source").get("code") for code_resp in json_response.get("hits").get("hits")]
+
+
+    @classmethod
+    def insert_data(cls, data: dict):
+        """
+        insert data to elastic search
+        """
+        
+if __name__ == "__main__":
+    print(ElasticAPI.data_isin(["k87335", "m30829858", "m5742256", "test"]))
+    
