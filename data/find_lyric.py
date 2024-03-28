8cfa2b4a6c7d16ea44aeac05bf48e27cff045d1c
heecircle <yhw991228@gmail.com>
2024-03-28 16:15:44 +0900

    feat : find lyric crawler and fix dir

--- data/find_lyric.py
+++ data/find_lyric.py
@@ -12,7 +12,8 @@ class Crawl:
         """
         get track id, artist name, track title from melon site
         """
-
+        
+        
         headers = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             + "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -24,16 +25,20 @@ class Crawl:
         response = requests.get(url, headers=headers, timeout=10)
         soup = bs(response.text)
         lyric = ""
-        for s in soup.find("div", attrs={"class": "lyric"}):
-            lyric += s.text.strip()
-            lyric += "\n"
-        lyric = lyric.replace("\n\n", "\n")
-
+        if(soup.find("div", attrs={"class": "lyric"}) is not None):
+            for s in soup.find("div", attrs={"class": "lyric"}):
+                lyric += s.text.strip()
+                lyric += "\n"
+            lyric = lyric.replace("\n\n", "\n").replace("\n\n", "\n").replace("\n\n", "\n")
+        print(track_id + " : " + lyric[:10] + "...")
         return lyric
 
 
 if __name__ == "__main__":
     app = Crawl()
-    df = pd.read_csv("data/melon_chart.csv")
-    df["lyric"] = df["track_id"].apply(lambda x: app.get_lyric(str(x)))
-    df.to_csv("data/melon_chart.csv", index=False)
+    df = pd.read_csv("data/melon/melon_chart.csv")
+    
+    df["lyric"] = df["code"].apply(lambda x: app.get_lyric(str(x)))
+    print()
+    print("===== Save to csv =====")
+    df.to_csv("data/melon/melon_chart.csv", index=False)
